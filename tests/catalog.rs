3e22dc170f4bd1be1ac6f3a46336d7b707983425
Dimitris Gianneler <greyelf1@gmail.com>
2022-06-21 00:41:40 +0300

    Cleanup catalog integration tests

--- tests/catalog.rs
+++ tests/catalog.rs
@@ -4,9 +4,16 @@ use indoc::indoc;
 use rstest::{fixture, rstest};
 use url::Url;
 
-use scryfall_sdk_rust::{Scryfall, ScryfallBlocking};
-use scryfall_sdk_rust::resources::catalog::Catalog;
-use scryfall_sdk_rust::resources::ResourceKind;
+use scryfall_sdk_rust::{
+    Scryfall,
+    ScryfallBlocking,
+    HttpResource,
+    resources::{
+        catalog::Catalog,
+        catalog::CatalogResource,
+        ResourceKind
+    }
+};
 
 #[fixture]
 #[once]
@@ -36,87 +43,79 @@ fn catalog() -> Catalog {
     }
 }
 
-// -- CatalogResource::AbilityWords tests
-mod ability_words {
-    use scryfall_sdk_rust::HttpResource;
-    use scryfall_sdk_rust::resources::catalog::CatalogResource;
+#[rstest]
+#[case::ability_words(CatalogResource::AbilityWords)]
+#[case::artifact_types(CatalogResource::ArtifactTypes)]
+#[case::artist_names(CatalogResource::ArtistNames)]
+#[case::card_names(CatalogResource::CardNames)]
+#[case::creature_types(CatalogResource::CreatureTypes)]
+#[case::enchantment_types(CatalogResource::EnchantmentTypes)]
+#[case::keyword_abilities(CatalogResource::KeywordAbilities)]
+#[case::keyword_actions(CatalogResource::KeywordActions)]
+#[case::land_types(CatalogResource::LandTypes)]
+#[case::loyalties(CatalogResource::Loyalties)]
+#[case::planeswalker_types(CatalogResource::PlaneswalkerTypes)]
+#[case::powers(CatalogResource::Powers)]
+#[case::spell_types(CatalogResource::SpellTypes)]
+#[case::toughnesses(CatalogResource::Toughnesses)]
+#[case::watermarks(CatalogResource::Watermarks)]
+#[case::word_bank(CatalogResource::WordBank)]
+fn test_blocking_request(response: &String, catalog: &Catalog, #[case]resource: CatalogResource) {
+    let server = MockServer::start();
 
-    use super::*;
+    let endpoint = server.mock(|when, then| {
+        when.method(GET).path(format!("/{}", resource.path()));
+        then.status(200)
+            .header("content-type", "application/json")
+            .body(response);
+    });
 
-    #[rstest]
-    #[case::ability_words(CatalogResource::AbilityWords)]
-    #[case::artifact_types(CatalogResource::ArtifactTypes)]
-    #[case::artist_names(CatalogResource::ArtistNames)]
-    #[case::card_names(CatalogResource::CardNames)]
-    #[case::creature_types(CatalogResource::CreatureTypes)]
-    #[case::enchantment_types(CatalogResource::EnchantmentTypes)]
-    #[case::keyword_abilities(CatalogResource::KeywordAbilities)]
-    #[case::keyword_actions(CatalogResource::KeywordActions)]
-    #[case::land_types(CatalogResource::LandTypes)]
-    #[case::loyalties(CatalogResource::Loyalties)]
-    #[case::planeswalker_types(CatalogResource::PlaneswalkerTypes)]
-    #[case::powers(CatalogResource::Powers)]
-    #[case::spell_types(CatalogResource::SpellTypes)]
-    #[case::toughnesses(CatalogResource::Toughnesses)]
-    #[case::watermarks(CatalogResource::Watermarks)]
-    #[case::word_bank(CatalogResource::WordBank)]
-    fn test_blocking_request(response: &String, catalog: &Catalog, #[case]resource: CatalogResource) {
-        let server = MockServer::start();
+    let url = server.base_url();
+    let client = ScryfallBlocking::from_url(&url);
 
-            let endpoint = server.mock(|when, then| {
-                when.method(GET).path(format!("/{}", resource.path()));
-                then.status(200)
-                    .header("content-type", "application/json")
-                    .body(response);
-            });
+    let response = client
+        .request(&resource)
+        .expect("Expected a valid Catalog response");
 
-        let url = server.base_url();
-        let client = ScryfallBlocking::from_url(&url);
+    endpoint.assert();
+    assert_eq!(catalog, &response)
+}
 
-        let response = client
-            .request(&resource)
-            .expect("Expected a valid BulkData response");
+#[rstest]
+#[case::ability_words(CatalogResource::AbilityWords)]
+#[case::artifact_types(CatalogResource::ArtifactTypes)]
+#[case::artist_names(CatalogResource::ArtistNames)]
+#[case::card_names(CatalogResource::CardNames)]
+#[case::creature_types(CatalogResource::CreatureTypes)]
+#[case::enchantment_types(CatalogResource::EnchantmentTypes)]
+#[case::keyword_abilities(CatalogResource::KeywordAbilities)]
+#[case::keyword_actions(CatalogResource::KeywordActions)]
+#[case::land_types(CatalogResource::LandTypes)]
+#[case::loyalties(CatalogResource::Loyalties)]
+#[case::planeswalker_types(CatalogResource::PlaneswalkerTypes)]
+#[case::powers(CatalogResource::Powers)]
+#[case::spell_types(CatalogResource::SpellTypes)]
+#[case::toughnesses(CatalogResource::Toughnesses)]
+#[case::watermarks(CatalogResource::Watermarks)]
+#[case::word_bank(CatalogResource::WordBank)]
+#[tokio::test]
+async fn test_async_request(response: &String, catalog: &Catalog, #[case]resource: CatalogResource) {
+    let server = MockServer::start_async().await;
 
-        endpoint.assert();
-        assert_eq!(catalog, &response)
-    }
+    let endpoint = server.mock(|when, then| {
+        when.method(GET).path(format!("/{}", resource.path()));
+        then.status(200)
+            .header("content-type", "application/json")
+            .body(response);
+    });
 
-    #[rstest]
-    #[case::ability_words(CatalogResource::AbilityWords)]
-    #[case::artifact_types(CatalogResource::ArtifactTypes)]
-    #[case::artist_names(CatalogResource::ArtistNames)]
-    #[case::card_names(CatalogResource::CardNames)]
-    #[case::creature_types(CatalogResource::CreatureTypes)]
-    #[case::enchantment_types(CatalogResource::EnchantmentTypes)]
-    #[case::keyword_abilities(CatalogResource::KeywordAbilities)]
-    #[case::keyword_actions(CatalogResource::KeywordActions)]
-    #[case::land_types(CatalogResource::LandTypes)]
-    #[case::loyalties(CatalogResource::Loyalties)]
-    #[case::planeswalker_types(CatalogResource::PlaneswalkerTypes)]
-    #[case::powers(CatalogResource::Powers)]
-    #[case::spell_types(CatalogResource::SpellTypes)]
-    #[case::toughnesses(CatalogResource::Toughnesses)]
-    #[case::watermarks(CatalogResource::Watermarks)]
-    #[case::word_bank(CatalogResource::WordBank)]
-    #[tokio::test]
-    async fn test_async_request(response: &String, catalog: &Catalog, #[case]resource: CatalogResource) {
-        let server = MockServer::start_async().await;
+    let url = server.base_url();
+    let client = Scryfall::from_url(&url);
 
-        let endpoint = server.mock(|when, then| {
-            when.method(GET).path(format!("/{}", resource.path()));
-            then.status(200)
-                .header("content-type", "application/json")
-                .body(response);
-        });
+    let response = client
+        .request(&resource).await
+        .expect("Expected a valid Catalog response");
 
-        let url = server.base_url();
-        let client = Scryfall::from_url(&url);
-
-        let response = client
-            .request(&resource).await
-            .expect("Expected a valid BulkData response");
-
-        endpoint.assert();
-        assert_eq!(catalog, &response)
-    }
+    endpoint.assert();
+    assert_eq!(catalog, &response)
 }
